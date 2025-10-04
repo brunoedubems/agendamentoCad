cf13a2620e1be89fbaf43197c764758a677033f6
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-10-04 09:42:28 -0300

    chore: configura o projeto

---
+++ src/main/java/br/com/brunoedubems/agendamento/AgendamentoDeEntrevistasApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunoedubems.agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeEntrevistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeEntrevistasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunoedubems/agendamento/AgendamentoDeEntrevistasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoedubems.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeEntrevistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
