a37a610617280247f0f4a420ac525fcbbeb93674
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-10-07 19:16:03 -0300

    refactor: modifica o nome do mapper e coloca a anotação de "@Transactional"

--- src/main/java/br/com/brunoedubems/agendamento/controller/EntrevistadorController.java
+++ src/main/java/br/com/brunoedubems/agendamento/controller/EntrevistadorController.java
@@ -4,8 +4,8 @@ import br.com.brunoedubems.agendamento.dto.Entrevistador.EntrevistadorRequest;
 import br.com.brunoedubems.agendamento.dto.Entrevistador.EntrevistadorResponse;
 import br.com.brunoedubems.agendamento.dto.Entrevistador.EntrevistadorResumo;
 import br.com.brunoedubems.agendamento.service.EntrevistadorService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -29,17 +29,16 @@ public class EntrevistadorController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EntrevistadorResponse> update( @PathVariable Long id, @RequestBody @Valid EntrevistadorResumo entrevistadorRequest) {
+    public ResponseEntity<EntrevistadorResponse> update( @PathVariable Long id,
+                                                         @RequestBody @Valid EntrevistadorResumo entrevistadorRequest) {
         return ResponseEntity.ok(entrevistadorService.update(id, entrevistadorRequest));
     }
 
-    @Transactional(readOnly = true)
     @GetMapping
     public ResponseEntity<List<EntrevistadorResponse>> findAll(){
         return ResponseEntity.ok(entrevistadorService.findAll());
     }
 
-    @Transactional(readOnly = true)
     @GetMapping("/{id}")
     public ResponseEntity<EntrevistadorResponse> findById(@PathVariable  Long id){
         return ResponseEntity.ok(entrevistadorService.findById(id));
--- src/main/java/br/com/brunoedubems/agendamento/service/EntrevistadorService.java
+++ src/main/java/br/com/brunoedubems/agendamento/service/EntrevistadorService.java
@@ -7,6 +7,7 @@ import br.com.brunoedubems.agendamento.entity.Entrevistador;
 import br.com.brunoedubems.agendamento.mapper.EntrevistadorMapper;
 import br.com.brunoedubems.agendamento.repository.EntrevistadorRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,41 +15,43 @@ import java.util.List;
 public class EntrevistadorService {
 
     private final EntrevistadorRepository repository;
-    private final EntrevistadorMapper entrevistadorMapper;
+    private final EntrevistadorMapper mapper;
 
-    public EntrevistadorService(EntrevistadorRepository repository, EntrevistadorMapper entrevistadorMapper) {
+    public EntrevistadorService(EntrevistadorRepository repository, EntrevistadorMapper mapper) {
         this.repository = repository;
-        this.entrevistadorMapper = entrevistadorMapper;
+        this.mapper = mapper;
     }
 
     public EntrevistadorResponse save(EntrevistadorRequest entrevistadorRequest) {
-        Entrevistador entrevistadorSalvo = repository.save(entrevistadorMapper.toEntity(entrevistadorRequest));
-        return entrevistadorMapper.toResponse(entrevistadorSalvo);
+        Entrevistador entrevistadorSalvo = repository.save(mapper.toEntity(entrevistadorRequest));
+        return mapper.toResponse(entrevistadorSalvo);
     }
 
     public EntrevistadorResponse update(Long id, EntrevistadorResumo entrevistadorRequest) {
         Entrevistador entity = repository.findById(id).orElseThrow(() -> new RuntimeException("Entrevistador não encontrado com id: " + id));
-        Entrevistador entidadeAtualizada = entrevistadorMapper.updateEntityFromDto(entrevistadorRequest, entity);
-        return entrevistadorMapper.toResponse(repository.save(entidadeAtualizada));
+        Entrevistador entidadeAtualizada = mapper.updateEntityFromDto(entrevistadorRequest, entity);
+        return mapper.toResponse(repository.save(entidadeAtualizada));
     }
 
-
+    @Transactional(readOnly = true)
     public List<EntrevistadorResponse> findAll() {
         return repository.findAll()
                 .stream()
-                .map(entrevistadorMapper::toResponse)
+                .map(mapper::toResponse)
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     public EntrevistadorResponse findById(Long id) {
         Entrevistador entrevistador = repository.findById(id).orElseThrow(() -> new RuntimeException("Entrevistador não encontrado com ID: " + id));
-        return entrevistadorMapper.toResponse(entrevistador);
+        return mapper.toResponse(entrevistador);
     }
 
     public void delete(Long id) {
         Entrevistador entrevistador = repository.findById(id).orElseThrow(() -> new RuntimeException("Entrevistador não encontrado com ID: " + id));
         entrevistador.setAtivo(false);
     }
+
 
 
 }
