31f31e54a276e6be342f52d622e758067e06f8fa
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-10-08 16:01:10 -0300

    fix: adiciona novo tratamento de exceção no handler global

--- src/main/java/br/com/brunoedubems/agendamento/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/brunoedubems/agendamento/exception/GlobalExceptionHandler.java
@@ -48,4 +48,8 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
 }
